889da0b59ea993a3387e37673d77ab92fcdd5130
omprakashsaggam <omprakash.saggam@gmail.com>
2026-01-20 13:54:45 +0100

    inital commit

---
+++ src/main/java/com/medibridge/sos_ambulance_service_medibridge/SosAmbulanceServiceMedibridgeApplication.java
@@ -0,0 +1,13 @@
+package com.medibridge.sos_ambulance_service_medibridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosAmbulanceServiceMedibridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SosAmbulanceServiceMedibridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medibridge/sos_ambulance_service_medibridge/SosAmbulanceServiceMedibridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medibridge.sos_ambulance_service_medibridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosAmbulanceServiceMedibridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
